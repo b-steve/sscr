01d97a42e095510a8450ad0253f306db4198412d
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-09-23 12:36:05 +0100

    Tests fixed.

--- tests/testthat/test-fits.r
+++ tests/testthat/test-fits.r
@@ -5,9 +5,9 @@ test_that(
     {
         compile.sscr()
         fit.pois <- fit.sscr(test.data$capt, test.data$traps, test.data$mask, "pois")
-        expect_that(max(abs(exp(fit.pois$par) - c(2.876451, 93.388203))) < 1e-5, is_true())
+        expect_that(max(abs(fit.pois - c(2.876451, 93.388203, 30.7750916, 0.4549133))) < 1e-5, is_true())
         fit.binom <- fit.sscr(test.data$bin.capt, test.data$traps, test.data$mask)
-        expect_that(max(abs(exp(fit.binom$par) - c(1.105443, 144.338956))) < 1e-5, is_true())
+        expect_that(max(abs(fit.binom - c(1.105443, 144.338956, 44.8268028, 0.3123132))) < 1e-5, is_true())
     })
 
 test_that(
@@ -23,4 +23,9 @@ test_that(
                              mask = test.data$mask, resp = "pois",
                              cov.structure = "exponential", test = TRUE)
         expect_that(test.exp - 122.9533 < 1e-4, is_true())
+        ## Full dependence (individual-level random effect).
+        ##test.full <- fit.sscr(capt = test.data$capt, traps = test.data$traps,
+        ##                      mask = test.data$mask, resp = "pois",
+        ##                      cov.structure = "full", test = TRUE, trace = TRUE)
+        ##expect_that(test.full - 0, is_true())
     })
