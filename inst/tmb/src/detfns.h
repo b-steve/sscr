c911d77335cc2a43ee055d70290fd5676e0830e9
Ben Stevenson <bcs5@st-andrews.ac.uk>
2016-10-07 16:01:46 +0100

    Adding file with detection functions.

---
+++ inst/tmb/src/detfns.h
@@ -0,0 +1,28 @@
+#ifndef DETFNS_H
+#define DETFNS_H
+
+template<class Type>
+Type detfn_hn (const Type &d, const vector<Type> &det_pars)
+{
+  return det_pars(0)*exp(-pow(d, 2)/(2*pow(det_pars(1), 2)));
+}
+
+template<class Type>
+Type detfn_hr (const Type &d, const vector<Type> &det_pars)
+{
+  return det_pars(0)*(1 - exp(-pow(d/det_pars(1), -det_pars(2))));
+}
+
+template<class Type>
+Type detfn (const Type &d, const vector<Type> &det_pars, const int &detfn_id)
+{
+  Type out;
+  if (detfn_id == 0){
+    out = detfn_hn(d, det_pars);
+  } else if (detfn_id == 1){
+    out = detfn_hr(d, det_pars);
+  }
+  return out;
+}
+
+#endif
