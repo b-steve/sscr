3855cc652c48d13d94ec31bd91f3aa9963ed7e7f
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2017-02-16 17:35:56 +1300

    Adding a couple of tests for Binomial fits.

--- tests/testthat/test-fits.r
+++ tests/testthat/test-fits.r
@@ -4,10 +4,15 @@ test_that(
     "Fits with no random effects.",
     {
         compile.sscr()
+        ## Poisson.
         fit.pois <- fit.sscr(test.data$capt, test.data$traps, test.data$mask, "pois")
         expect_that(max(abs(fit.pois - c(2.876451, 93.388203, 30.7750916, 0.4549133))) < 1e-4, is_true())
-        fit.binom <- fit.sscr(test.data$bin.capt, test.data$traps, test.data$mask)
-        expect_that(max(abs(fit.binom - c(1.105443, 144.338956, 44.8268028, 0.3123132))) < 1e-4, is_true())
+        ## Bernoulli.
+        fit.bern <- fit.sscr(test.data$bin.capt, test.data$traps, test.data$mask)
+        expect_that(max(abs(fit.bern - c(1.105443, 144.338956, 44.8268028, 0.3123132))) < 1e-4, is_true())
+        ## Binomial.
+        fit.binom <- fit.sscr(test.data$capt, test.data$traps, test.data$mask, "binom", 10)
+        expect_that(max(abs(fit.binom - c(0.5190142, 125.8786894, 56.4560761, 0.2479804))) < 1e-4, is_true())
     })
 
 test_that(
@@ -28,6 +33,11 @@ test_that(
                              mask = test.data$mask, resp = "pois",
                              cov.structure = "exponential", test = TRUE)
         expect_that(test.exp - 122.9533 < 1e-4, is_true())
+        ## ... with Bernoulli response.
+        test.bern <- fit.sscr(capt = test.data$bin.capt, traps = test.data$traps,
+                             mask = test.data$mask,
+                             cov.structure = "exponential", test = TRUE)
+        expect_that(test.bern - 78.60182 < 1e-4, is_true())
         ## Full dependence (individual-level random effect).
         ##test.full <- fit.sscr(capt = test.data$capt, traps = test.data$traps,
         ##                      mask = test.data$mask, resp = "pois",
